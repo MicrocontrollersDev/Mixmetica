af476422b3b9db51131b5f257538071db753d24f
MicrocontrollersDev <microcontrollersyt@gmail.com>
2023-07-04 16:34:21 -0700

    fix actions

--- src/main/java/com/github/microcontrollersdev/mixmetica/mixin/CapeUtilsMixin.java
+++ src/main/java/com/github/microcontrollersdev/mixmetica/mixin/CapeUtilsMixin.java
@@ -1,9 +1,11 @@
 package com.github.microcontrollersdev.mixmetica.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
+@Pseudo
 @Mixin(targets = "net.optifine.player.CapeUtils", remap = false)
 public class CapeUtilsMixin {
     @ModifyConstant(method = "downloadCape", constant = @Constant(stringValue = "http://s.optifine.net/capes/", ordinal = 0))
